b38291335a9c63198d1eb755bf293d4bc041d68c
Aapo Attila <aapo.attila@windowslive.com>
2021-02-24 12:13:52 +0200

    ES6 functions and gitignore update to ignore files

--- back/src/routes/upload.js
+++ back/src/routes/upload.js
@@ -4,27 +4,24 @@ const multer = require('multer');
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: ((req, file, cb) => {
     cb(null, '../front/files')
-  },
-  filename: function (req, file, cb) {
+  }),
+  filename: ((req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname)
-  },
+  }),
 })
 
 const upload = multer({ storage: storage }).single('file');
 
 router.post('/', (req, res) => {
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      console.error(err)
-      return res.status(500).json(err);
-    } else if (err) {
-      console.error(err)
+  upload(req, res, ((err) => {
+    if (err) {
+      console.error(err);
       return res.status(500).json(err);
     }
     return res.status(200).send(req.file)
-  })
+  }))
 });
 
 module.exports = router;
